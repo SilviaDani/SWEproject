3dcc936d8820754f682a1d30747eb65b794c1e46
SilviaDani <silvia.dani@stud.unifi.it>
2022-07-30 14:38:09 +0200

    Intarface of mainPage and logInPage

--- src/main/java/com/sweproject/main/Main.java
+++ src/main/java/com/sweproject/main/Main.java
@@ -7,8 +7,15 @@ import com.sweproject.model.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
-public class Main {
-    public static void main(String[] args) {
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    /*public static void main(String[] args) {
         Notifier foo = new Notifier();
         ArrayList<Subject> bar = new ArrayList<>();
         bar.add(foo.getSubject());
@@ -19,5 +26,18 @@ public class Main {
         Tracer tracer = new Tracer();
         tracer.createPrescription(foo.getSubject(), new Date(LocalDateTime.of(2000,9,7,11,20)), new CovidTest(CovidTestType.MOLECULAR));
 
+    }*/
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
+
