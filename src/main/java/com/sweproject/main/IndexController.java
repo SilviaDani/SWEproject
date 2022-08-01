187664aa51ae557f5f785d2613b6f46d298a32e8
NiccoloNiccoli <niccolo.niccoli@stud.unifi.it>
2022-08-01 17:57:10 +0200

    changed buttons

--- src/main/java/com/sweproject/main/IndexController.java
+++ src/main/java/com/sweproject/main/IndexController.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
+import javafx.event.ActionEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
@@ -19,7 +19,7 @@ public class IndexController {
     private Scene scene;
     private Parent root;
 
-    public void logInClicked(MouseEvent event) throws IOException{
+    public void logInClicked(ActionEvent event) throws IOException{
         Parent root = FXMLLoader.load(getClass().getResource("logInPage.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
@@ -27,7 +27,7 @@ public class IndexController {
         stage.show();
     }
 
-    public void signInClicked(MouseEvent event) {
+    public void signInClicked(ActionEvent event) {
     }
 
 
