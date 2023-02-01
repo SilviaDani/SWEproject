b26e538eee449fbbba8015b6f3034229d57a18cb
SilviaDani <silvia.dani@stud.unifi.it>
2023-02-01 16:04:25 +0100

    metodo vecchio

--- src/main/java/com/sweproject/analysis/STPNAnalyzer_ext.java
+++ src/main/java/com/sweproject/analysis/STPNAnalyzer_ext.java
@@ -34,7 +34,111 @@ public class STPNAnalyzer_ext<R,S> extends STPNAnalyzer{
     public STPNAnalyzer_ext(int samples, int step) {
         super(samples, step);
     }
+
     public TransientSolution<R,S> makeModel(String fiscalCode, ArrayList<HashMap<String, Object>> environmentArrayList, ArrayList<HashMap<String, Object>> testArrayList, ArrayList<HashMap<String, Object>> symptomsArrayList) throws Exception {
+        for(int testIndex = 0; testIndex < testArrayList.size(); testIndex++){
+            String rawType = (String) testArrayList.get(testIndex).get("type");
+            String[] extractedType = rawType.split("-"); // 0 -> Covid_test, 1 -> type of test, 2 -> outcome
+            testArrayList.get(testIndex).put("testType", extractedType[1].equals("MOLECULAR")? CovidTestType.MOLECULAR:CovidTestType.ANTIGEN);
+            testArrayList.get(testIndex).put("isPositive", extractedType[2].equals("true"));
+        }
+        LocalDateTime endInterval = LocalDateTime.now();
+        LocalDateTime now = endInterval.minusDays(6);
+        boolean showsSymptoms = false;
+        if (environmentArrayList.size() > 0){
+            for (int contact = 0; contact < environmentArrayList.size(); contact++){
+                showsSymptoms = false;
+                LocalDateTime contact_time = (LocalDateTime) environmentArrayList.get(contact).get("start_date");
+                float risk_level = (float) environmentArrayList.get(contact).get("risk_level");
+                double cumulativeRiskLevel = risk_level;
+                if (symptomsArrayList.size() > 0){
+                    for (int symptom = 0; symptom < symptomsArrayList.size(); symptom++){
+                        LocalDateTime symptom_date = (LocalDateTime) symptomsArrayList.get(symptom).get("start_date");
+                        if (contact_time.isBefore(symptom_date)){
+                            Symptoms symptoms = new Symptoms();
+                            cumulativeRiskLevel += symptoms.updateEvidence(contact_time, symptom_date);
+                            showsSymptoms = true;
+                        }
+                    }
+                }
+                if (testArrayList.size() > 0){
+                    for (int test = 0; test < testArrayList.size(); test++){
+                        LocalDateTime test_time = (LocalDateTime) testArrayList.get(test).get("start_date");
+                        if (contact_time.isBefore(test_time)){
+                            CovidTest covidTest = new CovidTest((CovidTestType) testArrayList.get(test).get("testType"), (boolean) testArrayList.get(test).get("isPositive"));
+                            System.out.println("Covid CCC" + covidTest.getName());
+                            double testEvidence = covidTest.isInfected(contact_time, test_time);
+                            System.out.println(testEvidence);
+                            cumulativeRiskLevel+=testEvidence;
+                        }
+                    }
+                }
+                cumulativeRiskLevel /= (symptomsArrayList.size() + testArrayList.size() + 1);
+                System.out.println(cumulativeRiskLevel + " prima");
+                cumulativeRiskLevel = updateRiskLevel(cumulativeRiskLevel, contact_time, showsSymptoms);
+                System.out.println(cumulativeRiskLevel + " dopo");
+                environmentArrayList.get(contact).replace("risk_level", risk_level, (float) cumulativeRiskLevel);
+            }
+            PetriNet net = new PetriNet();
+            Marking marking = new Marking();
+            //Generating Nodes
+            Place Contagio = net.addPlace("Contagio");
+            buildContagionEvolutionSection(net, marking, Contagio);
+            Place p1 = net.addPlace("Condizione iniziale");
+            Place p2 = net.addPlace("Primo incontro");
+            marking.setTokens(p1, 1);
+            Transition t0 = net.addTransition("t0");
+            Transition e0 = net.addTransition("effective0");
+            Transition u0 = net.addTransition("uneffective0");
+            float delta = (float) ChronoUnit.MINUTES.between(now, (LocalDateTime) environmentArrayList.get(0).get("start_date")) / 60.f;
+            float newRiskLevel = addTimeRelevance(delta, (float) environmentArrayList.get(0).get("risk_level"));
+            t0.addFeature(StochasticTransitionFeature.newDeterministicInstance(new BigDecimal(delta)));
+            e0.addFeature(StochasticTransitionFeature.newDeterministicInstance(new BigDecimal("0"), MarkingExpr.from(String.valueOf(newRiskLevel), net)));
+            u0.addFeature(StochasticTransitionFeature.newDeterministicInstance(new BigDecimal("0"), MarkingExpr.from(String.valueOf(1 - newRiskLevel), net)));
+            net.addPrecondition(p1, t0);
+            net.addPostcondition(t0, p2);
+            net.addPrecondition(p2, e0);
+            net.addPostcondition(e0, Contagio);
+            net.addPrecondition(p2, u0);
+
+            Transition lastTransition = u0;
+            for (int i = 1; i < environmentArrayList.size(); i++) {
+                Place p3 = net.addPlace("Dopo incontro " + i);
+                Place p4 = net.addPlace("Incontro " + (i + 1));
+                Transition t1 = net.addTransition("t " + i), e1 = net.addTransition("effective " + i), u1 = net.addTransition("uneffective " + i);
+                delta = (float) ChronoUnit.MINUTES.between((LocalDateTime) environmentArrayList.get(i - 1).get("start_date"), (LocalDateTime) environmentArrayList.get(i).get("start_date")) / 60.f;
+                newRiskLevel = addTimeRelevance(delta, (float) environmentArrayList.get(i).get("risk_level"));
+                t1.addFeature(StochasticTransitionFeature.newDeterministicInstance(new BigDecimal(delta)));
+                e1.addFeature(StochasticTransitionFeature.newDeterministicInstance(new BigDecimal("0"), MarkingExpr.from(String.valueOf(newRiskLevel), net)));
+                u1.addFeature(StochasticTransitionFeature.newDeterministicInstance(new BigDecimal("0"), MarkingExpr.from(String.valueOf(1 - newRiskLevel), net)));
+                net.addPostcondition(lastTransition, p3);
+                net.addPrecondition(p3, t1);
+                net.addPostcondition(t1, p4);
+                net.addPrecondition(p4, e1);
+                net.addPostcondition(e1, Contagio);
+                net.addPrecondition(p4, u1);
+                lastTransition = u1;
+            }
+            // 144 -> 6 giorni
+            RegTransient analysis = RegTransient.builder()
+                    .greedyPolicy(new BigDecimal(samples), new BigDecimal("0.001"))
+                    .timeStep(new BigDecimal(step)).build();
+
+            //TODO: add plots of other rewards and change title
+            //If(Contagioso>0&&Sintomatico==0,1,0);Contagioso;Sintomatico;If(Guarito+Isolato>0,1,0)
+            var rewardRates = TransientSolution.rewardRates("Contagioso");
+
+            TransientSolution<DeterministicEnablingState, Marking> solution =
+                    analysis.compute(net, marking);
+
+            var rewardedSolution = TransientSolution.computeRewards(false, solution, rewardRates);
+            return (TransientSolution<R, S>) rewardedSolution;
+        }else{
+            System.out.println("The subject has no 'Environment' observations of the last 6 days");
+            return makeFakeNet();
+        }
+    }
+    public TransientSolution<R,S> makeModel2(String fiscalCode, ArrayList<HashMap<String, Object>> environmentArrayList, ArrayList<HashMap<String, Object>> testArrayList, ArrayList<HashMap<String, Object>> symptomsArrayList) throws Exception {
 
         for(int testIndex = 0; testIndex < testArrayList.size(); testIndex++){
             String rawType = (String) testArrayList.get(testIndex).get("type");
@@ -42,8 +146,6 @@ public class STPNAnalyzer_ext<R,S> extends STPNAnalyzer{
             testArrayList.get(testIndex).put("testType", extractedType[1].equals("MOLECULAR")? CovidTestType.MOLECULAR:CovidTestType.ANTIGEN);
             testArrayList.get(testIndex).put("isPositive", extractedType[2].equals("true"));
         }
-
-
         LocalDateTime endInterval = LocalDateTime.now();
         LocalDateTime now = endInterval.minusDays(6);
         boolean showsSymptoms = false;
@@ -109,7 +211,7 @@ public class STPNAnalyzer_ext<R,S> extends STPNAnalyzer{
         }
     }
 
-    public <S, R> XYChart.Series makeChart(TransientSolution<S, R> s, ArrayList<HashMap<String, Object>> eventsArrayList, LocalDateTime pastStartTime){
+    public <S, R> XYChart.Series makeChart2(TransientSolution<S, R> s, ArrayList<HashMap<String, Object>> eventsArrayList, LocalDateTime pastStartTime){
         XYChart.Series<String, Float> series = new XYChart.Series();
         int size = s.getSamplesNumber();
         for (int i = 0; i < size; i++){
